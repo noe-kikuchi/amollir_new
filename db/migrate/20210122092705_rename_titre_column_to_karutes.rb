dcdef14d96e4a3a9af1b9f1ce2247dfe3946e33c
noe-kikuchi <noenoe888815@gmail.com>
2021-01-23 23:25:46 +0900

    マイグレーションファイル削除

--- db/migrate/20210122092705_rename_titre_column_to_karutes.rb
+++
@@ -1,5 +0,0 @@
-class RenameTitreColumnToKarutes < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :karutes, :user_id, :patient_id
-  end
-end
--- db/migrate/20210123064447_karutes.rb
+++
@@ -1,5 +0,0 @@
-class Karutes < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :Karutes
-  end
-end
